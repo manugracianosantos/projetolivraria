b5b58b620dac4279fe9c0aa046cacca344bb918c
Manuella Graciano <manulugraci@gmail.com>
2024-10-31 14:58:14 -0300

    página de compras

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,39 @@ $(document).ready(function () {
         ]
     });
 });
+$(document).ready(function () {
+    $('.second-js-slider').slick({
+        
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+    
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+    });
+});
